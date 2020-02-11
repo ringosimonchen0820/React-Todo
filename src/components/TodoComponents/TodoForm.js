f4c03c7f2db8b432e3c5a7eefe749ef223706ddf
ringosimonchen0820 <ringosimonchen0820@gmail.com>
2020-02-10 20:14:19 -0500

    setup TodoForm component

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+class TodoForm extends Component {
+    constructor() {
+        super();
+        this.state = {
+            newTodo: ''
+        };
+    }
+
+    // class property to handle search bar state changes
+    handleChanges = event => {
+        //* update state with each keystroke
+        this.setState({
+            newTodo: event.target.value
+        });
+        console.log(event.target.value);
+    }
+
+    // class property to submit form
+    handleSubmit = event => {
+        event.preventDefault();
+        //? this.setState({ newTodo: '' });
+    }
+
+    render() {
+        console.log('rendering form');
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    type='text'
+                    name='newTodo'
+                    value={this.state.newTodo}
+                    onChange={this.handleChanges}
+                />
+            </form>
+        )
+    }
+}
+
+export default TodoForm;
